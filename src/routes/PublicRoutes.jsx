e5659d4c71fd657e2854dd524a3d45a929eecbc6
Abbas Shaik <shaikabbas101@gmail.com>
2025-11-22 16:48:44 +0530

    initial working ui

--- src/routes/PublicRoutes.jsx
+++ src/routes/PublicRoutes.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-
 import Login from "../pages/Auth/Login";
 import Register from "../pages/Auth/Register";
 import Dashboard from "../pages/Dashboard";
 import Goals from "../pages/Goals";
-import Reminders from "../pages/Reminders";
 import ProviderView from "../pages/ProviderView";
 import HealthTips from "../pages/HealthTips";
-import ProtectedRoute from "./ProtectedRoute";
+import Reminders from "../pages/Reminder";
+import ProtectedRoute from "./ProtectedRoutes";
 
 export default function PublicRoutes() {
   return (
