e0907513d8d6ded07c8e59824e6ba6e56688c703
Abbas Shaik <shaikabbas101@gmail.com>
2025-11-22 15:10:30 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-blue-500">Tailwind Test</h1>
+    </div>
+  );
+}
+
+export default App;
